fbceae707f865f34f2f9a3f46d3527cb62c5eee1
aeither <36173828+aeither@users.noreply.github.com>
2022-09-05 17:57:56 +0200

    Initial example

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,8 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "contracts/token/TokenERC20.sol";
+import "@thirdweb-dev/contracts/token/TokenERC20.sol";
 
 contract Contract is TokenERC20 {
     constructor() {}
+
+    fallback() external payable {}
+
+    receive() external payable {}
+
+    function mintWithWei() public payable {
+        // only minter can mint
+        // mintTo(msg.sender, msg.value);
+
+        _mint(msg.sender, msg.value);
+    }
 }
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,11 +2,17 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import {Contract} from "src/Contract.sol";
 
 contract ContractTest is Test {
-    function setUp() public {}
+    Contract myContract;
+
+    function setUp() public {
+        myContract = new Contract();
+    }
 
     function testExample() public {
-        assertTrue(true);
+        myContract.mintWithWei{value: 1 wei}();
+        assertEq(myContract.balanceOf(address(this)), 1 wei);
     }
 }
